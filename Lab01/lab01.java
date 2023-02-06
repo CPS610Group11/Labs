bd4046a4e52020e469c446abc1eb1f180cefc877
Adam Whittington <96022012+AWhittington512@users.noreply.github.com>
2023-02-06 18:45:45 -0500

    Update lab01.java

--- Lab01/lab01.java
+++ Lab01/lab01.java
@@ -1,4 +1,3 @@
-package Lab01;
 import java.io.*;
 import java.sql.*;
 import java.util.Scanner;
@@ -9,7 +8,8 @@ class calGPA
         try
         {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-        }catch (ClassNotFoundException x)
+        }
+        catch (ClassNotFoundException x)
         {
             System.out.println("Driver could not be loaded.");
         }
@@ -27,18 +27,18 @@ class calGPA
         ResultSet r = p.executeQuery();
         double count=0, sum=0, avg=0;
         while(r.next())
-            {
+        {
             grade = r.getString(1).charAt(0);
             credit = r.getInt(2);
             switch (grade)
-        {
-            case 'A': sum=sum+(4*credit); count=count+1; break;
-            case 'B': sum=sum+(3*credit); count=count+1; break;
-            case 'C': sum=sum+(2*credit); count=count+1; break;
-            case 'D': sum=sum+(1*credit); count=count+1; break;
-            case 'F': sum=sum+(0*credit); count=count+1; break;
-            default: System.out.println("This grade "+grade+" will not be calculated."); break;
-        }
+            {
+                case 'A': sum=sum+(4*credit); count=count+1; break;
+                case 'B': sum=sum+(3*credit); count=count+1; break;
+                case 'C': sum=sum+(2*credit); count=count+1; break;
+                case 'D': sum=sum+(1*credit); count=count+1; break;
+                case 'F': sum=sum+(0*credit); count=count+1; break;
+                default: System.out.println("This grade "+grade+" will not be calculated."); break;
+            }
         };
         avg = sum/count;
         System.out.println("Student named "+name+" has a grade point average "+avg+".");
